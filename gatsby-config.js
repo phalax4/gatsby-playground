ef8a26a77758dd0de54dacbf201376043127fb67
Jeremy Lim <4392084+phalax4@users.noreply.github.com>
2020-06-09 17:53:48 -0500

    Fixing url

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@ module.exports = {
     title: `Gatsby Starter Personal Blog`,
     author: `Gatsby`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://phalax4.github.io`,
+    pathPrefix:"/gatsby-playground",
     social: {
       twitter: `gatsbyjs`,
     },
